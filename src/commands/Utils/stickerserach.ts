710129880a57a419e1757a3751dbb085353d597c
deepak260901 <105791403+deepak260901@users.noreply.github.com>
2022-06-02 07:20:37 +0530

    Update stickerserach.ts

--- src/commands/Utils/stickerserach.ts
+++ src/commands/Utils/stickerserach.ts
@@ -33,7 +33,7 @@ const b = `${data.results?.[Math.floor(Math.random() * data.results.length)]?.me
 
         const sticker: any = await new Sticker(b, {
 			pack: "sticker",
-			author: "𝙇𝙤𝙧𝙙 𝙎𝙖𝙫𝙖𝙜𝙚 ⚡ 𝑳𝒐𝒓𝒅 𝑺𝒂𝒗𝒂𝒈𝒆 ⚡",
+			author: "𝙴𝚕𝚊𝚒𝚗𝚊 ⚡ 𝔾𝕒𝕡𝕡𝕦 ⚡",
 			quality: 90,
 			type: "crop",
 			categories: ["🎊"],
