e704294095fd63b5d409a3e754147fad9582c7d7
deepak260901 <105791403+deepak260901@users.noreply.github.com>
2022-06-02 07:02:06 +0530

    Update Savage.ts

--- src/commands/General/Savage.ts
+++ src/commands/General/Savage.ts
@@ -8,8 +8,8 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'savage',
-            description: 'Displays info about savage.',
+            command: 'gappu',
+            description: 'Displays info about gappu.',
             category: 'general',
             usage: `${client.config.prefix}savage`
         })
@@ -22,15 +22,13 @@ export default class Command extends BaseCommand {
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.image, {quoted:M.WAMessage,
             mimetype: Mimetype.jpeg,
-            caption: `🍭𝗵𝗲𝗹𝗹𝗼!🍃I'm Lord Savage 🕊️ a learner/student & an upcoming developer in the future.
+            caption: `🍭𝗵𝗲𝗹𝗹𝗼!🍃I'm Deepak 🕊️.
             
 🍁𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥;
-Wa.me/+263716928420
-Wa.me/+263716564623
+Wa.me/+919015286428
 
 📮𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢;
-https://instagram.com/_smokey_vines_
-https://instagram.com/amapiano_state
+https://instagram.com/_g.a.p.p.u_
 
 🚀𝘿𝙞𝙨𝙘𝙤𝙧𝙙;
 ｟𝖢𝗈𝗆𝗂𝗇𝗀 𝖲𝗈𝗈𝗇｠
